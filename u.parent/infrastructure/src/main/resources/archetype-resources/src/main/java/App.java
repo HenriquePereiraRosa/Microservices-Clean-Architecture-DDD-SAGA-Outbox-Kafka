f355ba198756d12c96968adf3e56d1992387fd48
Henrique Pereira Rosa <henrique.prosa@hotmail.com>
2022-11-07 23:08:30 +0000

    feat.0010_class27: added KAFKA docker-compose files

---
+++ u.parent/infrastructure/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.h.udemy.java.uservices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
