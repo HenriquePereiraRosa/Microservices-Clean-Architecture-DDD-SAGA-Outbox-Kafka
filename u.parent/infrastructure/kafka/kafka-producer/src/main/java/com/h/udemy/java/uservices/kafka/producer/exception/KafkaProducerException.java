fb10466c1d6a141f5563a4d6332031004c35a252
Henrique Pereira Rosa <henrique.prosa@hotmail.com>
2022-11-09 13:53:32 +0000

    feat14_class30: added Consumer and Producer's config

---
+++ u.parent/infrastructure/kafka/kafka-producer/src/main/java/com/h/udemy/java/uservices/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,8 @@
+package com.h.udemy.java.uservices.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
